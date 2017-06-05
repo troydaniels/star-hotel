115d2240244c87b78dc0d17c571c968bb9b9de13
Troy Daniels <troyaadaniels@gmail.com>
2017-06-06 09:21:58 +1000

    Change preamble

--- starhotel/Login.java
+++ starhotel/Login.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/* 32555 Star Hotel Assessment
+ *
+ * Shruti Rane 12760878, Anzhao Wang 12737678, Troy Daniels 12957130
+*/
 package starhotel;
 
 import javax.swing.*;    
